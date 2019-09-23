d466c216c6659548fa17b93a41f6f6626674dcb8
Gabriele <gvirga@student.s19.be>
2019-09-23 23:14:53 +0200

    ex 13 done, checking from 13 to 10 done

--- D05/ex01/ex01.sql
+++ D05/ex01/ex01.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS `ft_table` (
-    id INT AUTO_INCREMENT PRIMARY_KEY,
-    login VARCHAR(8) DEFAULT NOT NULL 'toto',
-    group ENUM('staff', 'student', 'other') NOT NULL,
-    creation_date DATE NOT NULL
+CREATE TABLE ft_table
+(
+  id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+  login VARCHAR(8) NOT NULL DEFAULT 'toto',
+  groupe ENUM('staff', 'student', 'other') NOT NULL,
+  creation_date DATE NOT NULL
 )
---
+++ D05/ex06/ex06.sql
@@ -0,0 +1,5 @@
+SELECT summary,title 
+FROM film
+WHERE summary
+LIKE "%Vincent%"
+ORDER BY id_film ASC;
